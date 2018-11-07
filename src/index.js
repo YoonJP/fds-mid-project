56d7f8410ddc88e69915ef2b2734a272ea5c239c
Yoon jae Park <pyoonj0953@gmail.com>
2018-11-07 17:46:07 +0900

    add logo img (not completed)

--- src/index.js
+++ src/index.js
@@ -17,8 +17,8 @@ api.interceptors.request.use(function (config) {
 
 const templates = {
   loginForm: document.querySelector('#login-form').content,
-  postList: document.querySelector('#post-list').content,
-  postItem: document.querySelector('#post-item').content,
+  productList: document.querySelector('#product-list').content,
+  productItem: document.querySelector('#product-item').content
 
 }
 
@@ -52,8 +52,9 @@ async function drawLoginForm() {
       username,
       password
     })
+
     localStorage.setItem('token', res.data.token)
-    drawPostList()
+    drawProductList()
   })
 
   // 6. 템플릿을 문서에 삽입
@@ -62,33 +63,31 @@ async function drawLoginForm() {
   console.log('drawLoginForm well done')
 }
 
-// drawLoginForm()
-
 // Function to draw post list
-async function drawPostList() {
+async function drawProductList() {
   // 1. 템플릿 복사
-  const frag = document.importNode(templates.postList, true)
+  const frag = document.importNode(templates.productList, true)
 
   // 2. 요소 선택
-  const listEl = frag.querySelector('.post-list')
+  const listEl = frag.querySelector('.product-list')
 
   // 3. 필요한 데이터 불러오기
-  const {data: postList} = await api.get('/posts?_expand=user')
+  const { data: productList } = await api.get('/products?_expand=user')
 
   // 4. 내용 채우기
-  for (const postItem of postList) {
-    const frag = document.importNode(templates.postItem, true)
+  for (const productItem of productList) {
+    const frag = document.importNode(templates.productItem, true)
 
     const idEl = frag.querySelector('.id')
-    const titleEl = frag.querySeletor('.title')
-    const authorEl = frag.quereySelector('.author')
+    const titleEl = frag.querySelector('.title')
+    // const authorEl = frag.quereySelector('.author')
 
-    idEl.textContent = postItem.idEl
-    titleEl.textContent = postItem.title
-    authorEl.textContent = postItem.user.username
+    idEl.textContent = productItem.idEl
+    titleEl.textContent = productItem.title
+    // authorEl.textContent = productItem.user.username
 
     titleEl.addEventListener('click', e => {
-      drawPostDetail(postItem.id)
+      drawProductDetail(productItem.id)
     })
 
     listEl.appendChild(frag)
@@ -98,52 +97,12 @@ async function drawPostList() {
   // 6. 템플릿을 문서에 삽입
   rootEl.textContent = ''
   rootEl.appendChild(frag)
-  console.log('drawPostList well done')
+  console.log('drawProductList well done')
 }
-
-// // 1. 템플릿 복사
-// const frag = document.importNode(templates.postList, true)
-
-// // 2. 요소 선택
-// const listEl = frag.querySelector('.post-list')
-// const createEl = frag.querySelector('.create')
-
-// // 3. 필요한 데이터 불러오기
-// const { data: postList } = await api.get('/posts?_expand=user')
-// // const res = await api.get('/posts?_embed=user')
-// // const postList = res.data
-
-// // 4. 내용 채우기
-// for (const postItem of postList) {
-//   const frag = document.importNode(templates.postItem, true)
-
-//   const idEl = frag.querySelector('.id')
-//   const titleEl = frag.querySelector('.title')
-//   const authorEl = frag.querySelector('.author')
-
-//   idEl.textContent = postItem.id
-//   titleEl.textContent = postItem.title
-//   authorEl.textContent = postItem.user.username
-
-//   titleEl.addEventListener('click', e => {
-//     drawPostDetail(postItem.id)
-//   })
-
-//   listEl.appendChild(frag)
-// }
-
-// // 5. 이벤트 리스너 등록하기
-// createEl.addEventListener('click', e => {
-//   drawNewPostForm()
-// })
-
-// // 6. 템플릿을 문서에 삽입
-// rootEl.textContent = ''
-// rootEl.appendChild(frag)
 
 // 페이지 로드 시 그릴 화면 설정
 if (localStorage.getItem('token')) {
-  drawPostList()
+  drawProductList()
 } else {
   drawLoginForm()
 }
